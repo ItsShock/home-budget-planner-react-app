f37a8ed808a9a02b2188d7151f012305f89c4f88
Piotr Domian <domianpiotr111@gmail.com>
2021-07-04 19:39:31 +0200

    Problem with total budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,14 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, expenses, incomes } = useContext(AppContext);
+    const { expenses, incomes } = useContext(AppContext);
 
     const totalBudget = () => {
         return (expenses.cost - incomes.cost)
     }
     return (
         <div className="border border-secondary border border-2 rounded">
-            <span className='ms-3'>You can spend: {budget - totalBudget}$</span>
+            <span className='ms-3'>You can spend: {totalBudget}$</span>
         </div>
     )
 }
